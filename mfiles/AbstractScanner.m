64829bedbba1d040ca8f00f450af60a7a8b44151
Electronic Kiwi <electron.kiwi@gmail.com>
2012-12-18 10:31:13 +0100

    AbstractScanner: added cell management methods.

--- mfiles/AbstractScanner.m
+++ mfiles/AbstractScanner.m
@@ -14,22 +14,28 @@ classdef AbstractScanner
     %   Date: 2012/12/17
     %   Copyright: 2012-2014, RWTH Aachen University
     
-    properties (Access = 'protected')
+    properties (Access = 'private')
         cells = [];
     end
 
     methods
-        function img_matrix = getCell(obj, x,y)
+        function img_matrix = getCell(obj, x, y)
             if ~isempty(obj.cells)
                 s = size(obj.cells);
                 if x > 0 && x < s(1)+1 && y > 0 && y < s(2)+1
-                    img_matrix = obj.cells(x,y,:,:);
+                    img_matrix = squeeze(obj.cells(x,y,:,:));
                 else
                     error('x or y is out of range! %d > x > 0 and %d > y > 0', s(1)+1, s(2)+1);
                 end
             else
                 error('No cell data available!');
             end
+        end
+    end
+
+    methods (Access = 'public')
+        function obj = addCell(obj, x, y, cell)
+            obj.cells(x,y,:,:) = cell;
         end
     end
 
