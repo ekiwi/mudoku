f1b3240b2d4550c7d9a40eea0e7e31944265b512
Maddimini <cryxgenx@googlemail.com>
2012-12-19 16:59:45 +0100

    changed values

--- mfiles/interpolation.m
+++ mfiles/interpolation.m
@@ -5,13 +5,13 @@ function interpolation(array)
 for i=1:height
     for j=1:width
         if(isnan(array(i,j)))
-            display('Found');
-            a = i;
+            ersteStelleNaN = i;
             while(isnan(array(i+1,j)))
                i=i+1;
             end
-            middle=(array(i+1,j)+array(a-1,j))*0.5;
-            for c=a:i
+            letzteStelleNaN = i;
+            middle=(array(letzteStelleNaN+1,j)+array(ersteStelleNaN-1,j))*0.5;
+            for c=ersteStelleNaN:letzteStelleNaN
                 array(c,j)=middle;
             end
         end
