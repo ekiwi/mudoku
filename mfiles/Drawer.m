5a1616113d0d1621768de31f8f4f127296adc3e5
Maddimini <cryxgenx@googlemail.com>
2012-12-19 15:54:37 +0100

    added class Drawer

---
+++ mfiles/Drawer.m
@@ -0,0 +1,22 @@
+classdef Drawer < GUIEnabledObject
+    % Drawer
+    %
+    % Syntax
+    %
+    % Description
+    %   Plots the numbers on the paper.
+    %
+    %
+    % Signature
+    %   Author: Johannes, Martin, Kevin, Florian
+    %   Date: 2012/12/19
+    %   Copyright: 2012-2014, RWTH Aachen University 
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
