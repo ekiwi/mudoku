0fb4211280f53ddbb5006856a7fcebc785f0c5a0
Maddimini <cryxgenx@googlemail.com>
2012-12-20 11:21:50 +0100

    fixed showEdges method

--- mfiles/showEdges.m
+++ mfiles/showEdges.m
@@ -1,16 +1,11 @@
 function [  ] = showEdges( array )
 
 I = mat2gray(array);
-subplot(1,3,1);
+subplot(1,2,1);
 imshow(I);
 
-h = fspecial('gaussian',10,10);
-I1 = imfilter(I,h);
-subplot(1,3,2);
-imshow(I1);
-
-subplot(1,3,3);
-Ibw = im2bw(I1,graythresh(I1));
+subplot(1,2,2);
+Ibw = im2bw(I,graythresh(I));
 imshow(Ibw);
 
 
